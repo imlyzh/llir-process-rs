446c39bc105d99f6a2bcf86762ee2bf96ea44d33
imlyzh <enterindex@gmail.com>
2021-08-11 22:07:35 +0800

    update main and readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # llir-process-rs
+
 This is a LLVM IR Process library
+
+- [x] parse llir
+- [x] to json
+- [ ] to sexpr
+- [ ] ast
+
+## 注意 | Notice
+
+由于Json没有保序的Object，所以我目前全部使用`Array<[Key, Value]>`格式存放数据
+
+## 示例 | Example
+
+```sh
+    llir_process ./demo.ir
+    [print json]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use crate::to_json::{ToJson, output_filter};
-
 mod ast;
 mod parser;
 mod to_json;
 
+use std::{env::args, io::Read};
+use std::fs::File;
+use crate::to_json::{ToJson, output_filter};
+
 fn main() {
-    let src = include_str!("./demo.ir");
-    let r = parser::parse(src).unwrap();
+    let r: Vec<String> = args().collect();
+    assert_eq!(r.len(), 2);
+    let src_file_path = r.get(1).unwrap();
+    let mut src_file = File::open(src_file_path).unwrap();
+    let mut src = String::new();
+    src_file.read_to_string(&mut src).unwrap();
+    // let src = include_str!("./demo.ir");
+    let r = parser::parse(&src).unwrap();
     let json = r.to_json();
     let json = output_filter(json);
-    println!("output json:\n{}", json.to_string());
+    println!("{}", json);
 }
